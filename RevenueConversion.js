8e57cafb3d86f765d5198093637e4e2d42dd47f6
John Hile <jkhile@gmail.com>
2017-04-29 13:04:34 -0400

    Replaced module commander with local, private @jkhile/args-config-prompt

--- RevenueConversion.js
+++ RevenueConversion.js
@@ -1,24 +1,22 @@
 // This is simple command-line interface to the main convertRevenue module.
 // Its use should be temporary until we get an Electron UI interface ready.
 // This module is simple ES5 so that no babel transpilation is necessary.
-
-const commander = require('commander')
+const ArgsConfigPrompt = require('@jkhile/args-config-prompt')
 const path = require('path')
 
 const {convertRevenue} = require('./lib/convertRevenue')
 
 function revenueConversion() {
-  commander
-    .option('-d, --dir [directory name]', 'name of base directory containing revenue reports')
-    .parse(process.argv)
-
-  if (!commander.hasOwnProperty('dir')) {
-    commander.help()
-    throw new Error('--directory option not specified')
-  }
-
+  const argOptions = [
+    {name: 'baseDirectory', alias: 'b'},
+    {name: 'company', alias: 'c', prompt: 'company to operate on, FEP or DCM'}
+  ]
+  const programArgs = new ArgsConfigPrompt(argOptions, 'fepdcm').resolve()
+  console.log(programArgs)
+  const revenueDirectory = path.resolve(programArgs.baseDirectory, `${programArgs.company.toUpperCase()}/RevenueReports`)
   try {
-    convertRevenue(path.resolve(commander.dir))
+    console.log('revenueDirectory', revenueDirectory)
+    convertRevenue(revenueDirectory)
   } catch (error) {
     console.log('OOPS! An error occurred while running RevenueConversion:')
     console.log(error)
